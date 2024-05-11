404f0487c1cfe183ffc2dc6dcb141b82d37e20cd
Sinh dev <mr.hanleve@gmail.com>
2024-05-11 21:19:56 +0700

    new

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,76 +1,106 @@
 <template>
-    <div class="login">
+    <div class="login" style="padding: 10px">
         <form @submit.prevent="login" class="formLogin">
-            <!-- Email input -->
-            <div class="notification">
-              Chào mừng bạn đến với Osiner.com
+            
+            <p class="notification">
+                Nhận Ngay Voucher 10% khi tạo tài khoản để chat
+            </p>
+            <div class="form-outline">
+                <input type="email" placeholder="Họ và Tên" class="form-control input" v-model="form.email" required
+                    autofocus />
             </div>
             <div class="form-outline">
-                <input type="email"  placeholder="Tên đăng nhập" class="form-control"
-                    v-model="form.email" required autofocus />
+                <input type="email" placeholder="Tên đăng nhập" class="form-control input" v-model="form.email" required
+                    autofocus />
             </div>
-            <!-- Password input -->
+                
             <div class="form-outline">
-                <input type="password" placeholder="Mật khẩu" class="form-control"
-                    v-model="form.password" required />
+                <input type="email" placeholder="Phòng Ban" class="form-control input" v-model="form.email" required
+                    autofocus />
             </div>
-            <!-- Submit button -->
-            <button type="submit" class="btn">Đăng nhập</button>
+           
+            <button type="submit" class="form-control btn">
+                Chat
+            </button>
+            <button type="submit" class="form-control btn">
+                Chat bằng tài khoản Facebook
+            </button>
+            <button type="submit" class="form-control btn">
+                Chat bằng tài khoản Google
+            </button>
         </form>
+        <!-- channel -->
+        <!-- <div class="channel_block">
+            <div class="channel_block_inner">
+                <p class="block-title">Liên hệ qua kênh yêu thích của bạn</p>
+                <div class="channel-items">
+                    <div class="channel-item"><a target="_blank" href="https://zalo.me/3485145501063282921"><img
+                                src="https://cdn.fchat.vn/assets/images/social/zalo.png">Zalo</a></div>
+                    <div class="channel-item"><a target="_blank" href="https://m.me/fchat.software/"><img
+                                src="https://cdn.fchat.vn/assets/images/social/message.png">Messenger</a></div>
+                    <div class="channel-item"><a href="tel:0898986008"><img
+                                src="https://cdn.fchat.vn/assets/images/social/phone.png">Hotline</a></div>
+                    <div class="channel-item"><a href="mailto:cskh@salemall.vn"><img
+                                src="https://cdn.fchat.vn/assets/images/social/email.png">Email</a></div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>  -->
+
     </div>
 </template>
 
 <script>
-import Swal from 'sweetalert2';
-import queryStr from '../libs/queryStr'
-export default {
-    name: 'LoginComponent',
-    data() {
-        return {
-            form: {
-                email: "",
-                password: "",
-                with_room_id: true,
-                page_id: "",
-            },
-            error: "",
-            data: [],
-        }
-    },
-    created() {
-console.log('thí.props', queryStr(this.$route.query.redirect));
-        
-    },
-    mounted() {
-        
-    },
-    methods: {
-        login() {
-            this.form.page_id = queryStr(this.$route.query.redirect).page_id
-            this.axios.post('/auth/login', this.form)
-            .then((res) => {
-                if(res.accessToken) {
-                    this.$root.$emit("login", true);
-                    localStorage.setItem('accessToken', res.accessToken);
-                    localStorage.setItem('roomId', res.roomId);
-                    localStorage.setItem('userData', JSON.stringify(res.userData));
-                    this.$router.push(this.$route.query.redirect);
-                }else {
-                    Swal.fire({
-                        title: 'OPPS',
-                        text: 'Username password not found 1!',
-                        icon: 'warning',
-                    });
-                }
-            }).catch((e) => {
-                console.log('error', e);
-                Swal.fire({
-                    title: 'OPPS',
-                    text: 'Username password not found 2!',
-                    icon: 'warning',
-                });
-            })
+    import Swal from 'sweetalert2';
+    import queryStr from '../libs/queryStr'
+    export default {
+        name: 'LoginComponent',
+        data() {
+            return {
+                form: {
+                    email: "",
+                    password: "",
+                    with_room_id: true,
+                    page_id: "",
+                },
+                error: "",
+                data: [],
+            }
         },
-    },
-}
+        created() {
+            console.log('thí.props', queryStr(this.$route.query.redirect));
+
+        },
+        mounted() {
+
+        },
+        methods: {
+            login() {
+                this.form.page_id = queryStr(this.$route.query.redirect).page_id
+                this.axios.post('/auth/login', this.form)
+                    .then((res) => {
+                        if (res.accessToken) {
+                            this.$root.$emit("login", true);
+                            localStorage.setItem('accessToken', res.accessToken);
+                            localStorage.setItem('roomId', res.roomId);
+                            localStorage.setItem('userData', JSON.stringify(res.userData));
+                            this.$router.push(this.$route.query.redirect);
+                        } else {
+                            Swal.fire({
+                                title: 'OPPS',
+                                text: 'Username password not found 1!',
+                                icon: 'warning',
+                            });
+                        }
+                    }).catch((e) => {
+                        console.log('error', e);
+                        Swal.fire({
+                            title: 'OPPS',
+                            text: 'Username password not found 2!',
+                            icon: 'warning',
+                        });
+                    })
+            },
+        },
+    }
 </script>
--- src/views/box-chat-widget/ChatWidget.vue
+++ src/views/box-chat-widget/ChatWidget.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
     <div v-if="room_id != null" className="chat-box">
+      <!-- product detail start -->
+      <div className="product-detail">
+        <img src="https://cf.shopee.vn/file/vn-11134207-7r98o-lq6ekru0wfyq14_tn" style="width:50px; height:50px" />
+        <div className="info">
+          <div className="title">ghế xoay làm việc văn phòng lưng dưới xoay tròn 360 độ cao cấp trường mau Sài Gòn ghế xoay làm việc văn phòng lưng dưới xoay tròn 360 độ cao cấp trường mau Sài Gòn</div>
+          <div className="price">
+            <span style="text-decoration:line-through;color:#bbb;padding-right:2px">500.000vnđ</span>
+            500.000vnđ</div>
+        </div>
+      </div>
+      <!-- product detail end -->
+      <!-- content chat start -->
       <div className="chat-container" ref="chatContainer">
-        <div v-for="(message, k) in messageList" :key="k" className="messageList">
-          <div :className="message.type === 'received' ? 'messages received' : 'messages sent'">
+        
+        <div v-for="(message, k) in messageList" :key="k" :className="message.type === 'received' ? 'messageList received' : 'messageList sent'">
+          <div className="messages">
             <div className="fullname" v-if="message.type === 'received'">{{ message.author}}</div>
             <div className="message">{{ message.data.text}}</div>
             <div className="status">{{ message.status }}</div>
           </div>
         </div>
-        <div v-if="userTyping" class="chat-typing">...  ai đó đang chat</div>
+        <div v-if="userTyping" class="chat-typing">... ai đó đang chat</div>
+        <div className="scrollToBottom"></div>
       </div>
+      <!-- content chat end -->
+      <!-- chat footer start -->
       <div className="chat-footer">
         <div class="chat-action">
-          <input class="input" placeholder="message..." v-model="newMessage" @keyup.enter="sendMessage" @keypress="typing"/><input class="submit" @click="sendMessage" type="submit"/>
+          <div class="input-attach"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M6 22h12a2 2 0 0 0 2-2V8l-6-6H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2zm7-18 5 5h-5V4zM8 14h3v-3h2v3h3v2h-3v3h-2v-3H8v-2z"></path></svg></div>
+      
+          <textarea class="input" placeholder="message..." rows=1 v-model="newMessage" @keyup.enter="sendMessage" @keypress="typing"></textarea>
+          <!-- <input class="submit" @click="sendMessage" type="submit"/> -->
+          <div class="input-smile"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M14.829 14.828a4.055 4.055 0 0 1-1.272.858 4.002 4.002 0 0 1-4.875-1.45l-1.658 1.119a6.063 6.063 0 0 0 1.621 1.62 5.963 5.963 0 0 0 2.148.903 6.042 6.042 0 0 0 2.415 0 5.972 5.972 0 0 0 2.148-.903c.313-.212.612-.458.886-.731.272-.271.52-.571.734-.889l-1.658-1.119a4.017 4.017 0 0 1-.489.592z"></path><circle cx="8.5" cy="10.5" r="1.5"></circle><circle cx="15.493" cy="10.493" r="1.493"></circle></svg></div>
+          
         </div>
-        <div class="chat-copyright">Power by Osiner.com</div>
-      </div>
+        <!-- <div class="chat-copyright">Power by Osiner.com</div> -->
+        </div>
     </div>
     <div v-else>
       <div style="text-align: center">
@@ -99,7 +120,7 @@ export default {
     typing() {
       this.sendSocket({
         pageId : this.page_id,
-        userId : this.userData.id,
+        userId : this.userData ? this.userData._id : null,
         appId  : this.app_id,
         ref     : this.ref,
         type    : 'typing',
@@ -116,10 +137,10 @@ export default {
     onSocketOpen() {
       //kiểm tra đã có phòng hay chưa? chưa thì tạo
       
-      console.log('connected socket');
+      //console.log('connected socket');
       this.sendSocket({
         pageId    : this.page_id,
-        userId    : this.userData._id,
+        userId    : this.userData ? this.userData._id : null,
         userData  : this.userData,
         appId     : this.app_id,
         ref       : this.ref,
@@ -194,7 +215,7 @@ export default {
         case 'room_id_osiner' : {
           //luu lai room_id
           //join vào phòng
-          console.log('room_id_osiner', jsonData.data.room_id)
+          //console.log('room_id_osiner', jsonData.data.room_id)
           this.room_id = jsonData.data.room_id
           localStorage.setItem('room_id', jsonData.data.room_id)
         }
@@ -224,7 +245,7 @@ export default {
           roomId: this.room_id,
           data: { text: this.newMessage, type: 'sent' },
           pageId: this.page_id,
-          userId: this.userData._id,
+          userId: this.userData ? this.userData._id : null,
           sort: this.sort+1
         }
         this.onMessageWasSent(data)
@@ -262,7 +283,7 @@ export default {
         this.$nextTick(() => {
             const chatContainer = this.$refs.chatContainer;
             if(typeof chatContainer !== 'undefined')
-            chatContainer.scrollTop = chatContainer.scrollHeight ? chatContainer.scrollHeight: 0
+            chatContainer.scrollTop = chatContainer.scrollHeight
         });
     },
     startButton() {
@@ -273,7 +294,7 @@ export default {
         type: 'start_osiner',
         appId: this.app_id,
         pageId: this.page_id,
-        userId : this.userData._id,
+        userId : this.userData ? this.userData._id : null,
       })
     }
   }
